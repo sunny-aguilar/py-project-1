b6d3db2ed362dada3b699a3ec0c31239d3d2c9b4
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-01 10:10:18 -0700

    practice

--- practice.py
+++ practice.py
@@ -10,7 +10,7 @@ average_passengers_per_car = passengers / cars_driven
 
 
 print("There are only", drivers, "drivers available")
-print("There will be", cars_not_driven, "empty cars today.")
+print("There will be", cars_not_driven, "empty cars today")
 print("We can transport", carpool_capacity, "people today.")
 print("We have", passengers, "to carpool today.")
 print("We need to put about", average_passengers_per_car, "in each car.")
